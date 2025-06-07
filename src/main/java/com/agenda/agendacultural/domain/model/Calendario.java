2ece413f5eb3d28565c8072f65d0a050bae1e945
CindyPessoa <cindypessoa17@gmail.com>
2025-06-07 19:54:46 -0300

    Calendario

---
+++ src/main/java/com/agenda/agendacultural/domain/model/Calendario.java
@@ -0,0 +1,13 @@
+package com.agenda.agendacultural.domain.model;
+
+import java.time.LocalDate;
+
+
+public class Calendario {
+
+    public LocalDate data;
+
+    public int horaFim;
+
+    public int horaInicio;
+}
