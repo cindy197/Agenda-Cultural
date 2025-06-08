ed13b5537d07d54c920ef3782bb65676cc9a0392
CindyPessoa <cindypessoa17@gmail.com>
2025-06-07 21:33:13 -0300

    Delete Evento, Listar todos, Listar por ID

--- src/main/java/com/agenda/agendacultural/domain/service/EventoService.java
+++ src/main/java/com/agenda/agendacultural/domain/service/EventoService.java
@@ -1,5 +1,6 @@
 package com.agenda.agendacultural.domain.service;
 
+import com.agenda.agendacultural.domain.exception.NotFoundException;
 import com.agenda.agendacultural.domain.model.Calendario;
 import com.agenda.agendacultural.domain.model.Evento;
 import com.agenda.agendacultural.domain.model.enums.EventoStatus;
@@ -7,10 +8,14 @@ import com.agenda.agendacultural.domain.repository.EventoRepository;
 import com.agenda.agendacultural.infraestructure.dto.EventoDTO;
 import com.agenda.agendacultural.infraestructure.mapper.CalendarioMapper;
 import com.agenda.agendacultural.infraestructure.mapper.EventoMapper;
+import jakarta.transaction.Transactional;
+import org.hibernate.query.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Pageable;
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class EventoService {
@@ -43,6 +48,21 @@ public class EventoService {
         });
 
         return eventoRepository.save(evento);
+    }
+
+
+    public Evento buscarPorId(String id) {
+        return eventoRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Evento não encontrado!"));
+    }
+
+    public void deletarEvento(String id) {
+        Evento evento = buscarPorId(id);
+        eventoRepository.delete(evento);
+    }
+
+    public List<Evento> buscarTodos() {
+        return eventoRepository.findAll();
     }
 
 }
--- src/main/java/com/agenda/agendacultural/infraestructure/controller/EventoController.java
+++ src/main/java/com/agenda/agendacultural/infraestructure/controller/EventoController.java
@@ -3,15 +3,17 @@ package com.agenda.agendacultural.infraestructure.controller;
 import com.agenda.agendacultural.domain.model.Evento;
 import com.agenda.agendacultural.domain.service.EventoService;
 import com.agenda.agendacultural.infraestructure.dto.EventoDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/eventos")
@@ -28,5 +30,26 @@ public class EventoController {
     @PostMapping
     public ResponseEntity<Evento> criarEvento(@RequestBody @Valid EventoDTO eventoDTO) {
         return new ResponseEntity<>(eventoService.salvar(eventoDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Buscar um evento por ID")
+    public ResponseEntity<Evento> buscarEventoId(@PathVariable String id) {
+        Evento evento = eventoService.buscarPorId(id);
+        return ResponseEntity.ok(evento);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Deletar um evento por ID")
+    public ResponseEntity<Void>deletarEvento(@PathVariable String id){
+        eventoService.deletarEvento(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    @Operation(summary = "Listar todos os eventos")
+    public ResponseEntity<List<Evento>> listarTodosEventos() { //Não vai ter o id, pois não vai filtrar por nada especifico
+        List<Evento> eventos = eventoService.buscarTodos();
+        return ResponseEntity.ok(eventos);
     }
 }
